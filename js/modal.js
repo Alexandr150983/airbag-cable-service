a5db982c5b3a6fbcc92e03b8924862a574096435
Fedorenko Oleksandr <oleksandrfedorenko.dev@gmail.com>
2025-02-24 17:04:50 +0200

    Add section

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openModalBtn.addEventListener('click', openModal);
+  refs.closeModalBtn.addEventListener('click', closeModal);
+
+  function openModal() {
+    refs.modal.classList.remove('is-hidden');
+    refs.body.style.overflow = 'hidden';
+  }
+
+  function closeModal() {
+    refs.modal.classList.add('is-hidden');
+    refs.body.style.overflow = 'auto';
+  }
+})();
